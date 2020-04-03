a00ded0f550f5f5a981dac79370d1eaef4db710d
Maci <maci1011@facebook.com>
2020-04-03 08:01:16 +0200

    general edits

--- Entity/User.php
+++ Entity/User.php
@@ -3,31 +3,52 @@
 namespace Maci\UserBundle\Entity;
 
 use FOS\UserBundle\Model\User as BaseUser;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * User
  */
 class User extends BaseUser
 {
-    /**
-     * @var integer
-     */
-    protected $id;
+	/**
+	 * @var integer
+	 */
+	protected $id;
 
+	/**
+	 * @Assert\NotBlank(groups={"Registration","Profile"})
+	 * @Assert\Regex(pattern="/^[a-zA-Z0-9_]+$/", message="validation.user.password.regex", groups={"Registration","Profile"})
+	 */
+	protected $username;
 
-    public function __construct()
-    {
-        parent::__construct();
-    }
+	/**
+	 * @Assert\Length(
+	 *     min=8,
+	 *     max=100,
+	 *     minMessage="validation.user.password.short",
+	 *     maxMessage="validation.user.password.too-long",
+	 *     groups={"Profile", "ResetPassword", "Registration", "ChangePassword"}
+	 * )
+	 * @Assert\Regex(
+	 *     pattern="/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).{8,100}$/",
+	 *     message="user.password.difficulty",
+	 *     groups={"Profile", "ResetPassword", "Registration", "ChangePassword"}
+	 * )
+	 */
+	protected $plainPassword;
 
-    /**
-     * Get id
-     *
-     * @return integer 
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
+	public function __construct()
+	{
+		parent::__construct();
+	}
 
+	/**
+	 * Get id
+	 *
+	 * @return integer 
+	 */
+	public function getId()
+	{
+		return $this->id;
+	}
 }
