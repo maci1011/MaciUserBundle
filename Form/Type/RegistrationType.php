c54a1db4411afeaa2a7606a9b8b7e4a0030fea3c
Maci <maci1011@facebook.com>
2022-07-06 02:06:44 +0200

    general edits

--- Form/Type/RegistrationType.php
+++ Form/Type/RegistrationType.php
@@ -10,24 +10,30 @@ use EWZ\Bundle\RecaptchaBundle\Validator\Constraints\IsTrue as RecaptchaTrue;
 
 class RegistrationType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $builder->add('recaptcha', EWZRecaptchaType::class, array(
-        	'label_attr'  => array('class'=> 'sr-only'),
-	        'mapped'      => false,
-			'constraints' => array(
-			    new RecaptchaTrue()
-			)
-        ));
-    }
+	protected $env;
 
-    public function getParent()
-    {
-        return 'FOS\UserBundle\Form\Type\RegistrationFormType';
-    }
+	public function __construct($env)
+	{
+		$this->env = $env;
+	}
 
-    public function getBlockPrefix()
-    {
-        return 'app_user_registration';
-    }
+	public function buildForm(FormBuilderInterface $builder, array $options)
+	{
+		if($options['env'] === "prod")
+			$builder->add('recaptcha', EWZRecaptchaType::class, [
+				'label_attr' => ['class'=> 'sr-only'],
+				'mapped' => false,
+				'constraints' => [new RecaptchaTrue()]
+			]);
+	}
+
+	public function getParent()
+	{
+		return 'FOS\UserBundle\Form\Type\RegistrationFormType';
+	}
+
+	public function getBlockPrefix()
+	{
+		return 'app_user_registration';
+	}
 }
